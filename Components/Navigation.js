05990070999e6eee8fc58c0e9a90c874bd0b14f3
Naveen <naveen.singisetty@xinthe.com>
2019-02-13 18:15:59 +0530

    Added new file

---
+++ Components/Navigation.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import { Container, Text, Button } from "native-base";
+import { Grid, Row } from "react-native-easy-grid";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import { Constants } from "expo";
+
+//First Screen Coponent
+class ScannerScreen extends React.Component {
+  static navigationOptions = {
+    header: null //Hide the header bar
+  };
+  render () {
+    return (
+      <Container style={{ marginTop: Constants.statusBarHeight }}>
+        <Grid
+          style={{
+            alignItems: "center"
+          }}
+        >
+
+          <Row>
+            <Text>Scanner Screen</Text>
+          </Row>
+          <Row>
+            <Button onPress={() => this.props.navigation.navigate("History")}>
+              <Text>History</Text>
+            </Button>
+            <Button onPress={() => this.props.navigation.navigate("Result")}>
+              <Text>Result</Text>
+            </Button>
+          </Row>
+        </Grid>
+      </Container>
+    );
+  }
+}
+
+//Second Screen component
+class HistoryScreen extends React.Component {
+  static navigationOptions = {
+    title: "History" //Set the header title
+  };
+  render() {
+    return (
+      <Container style={{ marginTop: Constants.statusBarHeight }}>
+        <Grid
+        style={{
+          alignItems: "center"
+        }}
+        >
+          <Row>
+            <Text>History Screen</Text>
+          </Row>
+        </Grid>
+      </Container>
+    );
+  }
+}
+
+//Third Screen Component
+class ResultScreen extends React.Component {
+  static navigationOptions = {
+    title: "Scan Detail" //Set the header title
+  };
+
+  render() {
+    return (
+      <Container style={{ marginTop: Constants.statusBarHeight }}>
+        <Grid
+          style={{
+            alignItems: "center"
+          }}
+          >
+            <Row>
+              <Text>Result Screen</Text>
+            </Row>
+          </Grid>
+        </Container>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Scanner: {
+      screen: ScannerScreen
+    },
+    History: {
+      screen: HistoryScreen
+    },
+    Result: {
+      screen: ResultScreen
+    }
+  },
+  {
+    initialRouteName: "Scanner" //Default sceeen name
+  }
+);
+export default createAppContainer(RootStack);
